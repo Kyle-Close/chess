2dc450cb56be27fa08c30ff1a1b1ae08ad401ce7
kyle-close <k.james.close@gmail.com>
2024-04-29 16:30:57 -0700

    save

--- src/components/board/index.tsx
+++ src/components/board/index.tsx
@@ -11,31 +11,29 @@ interface BoardProps {
 export function Board({ gameBoard }: BoardProps) {
   const { isWhite, isEmpty } = usePiece();
   return (
-    <>
-      <div className='flex grow relative'>
-        <Ranks />
-        <div className='flex flex-col grow relative'>
-          <div className='grid grid-cols-8 grid-rows-8 grow gap-1'>
-            {gameBoard.map((square, key) => {
-              const classes = [
-                'flex',
-                'justify-center',
-                'items-center',
-                'bg-zinc-500',
-              ];
-              if (!isWhite(square)) classes.push('text-black');
-              else if (isEmpty(square)) classes.push('text-amber-100');
+    <div className='flex grow relative'>
+      <Ranks />
+      <div className='flex flex-col grow relative'>
+        <div className='grid grid-cols-8 grid-rows-8 grow gap-1'>
+          {gameBoard.map((square, key) => {
+            const classes = [
+              'flex',
+              'justify-center',
+              'items-center',
+              'bg-zinc-500',
+            ];
+            if (!isWhite(square)) classes.push('text-black');
+            else if (isEmpty(square)) classes.push('text-amber-100');
 
-              return (
-                <div key={key} className={classes.join(' ')}>
-                  {getPieceAbbreviation(square)}
-                </div>
-              );
-            })}
-          </div>
-          <Files />
+            return (
+              <div key={key} className={classes.join(' ')}>
+                {getPieceAbbreviation(square)}
+              </div>
+            );
+          })}
         </div>
+        <Files />
       </div>
-    </>
+    </div>
   );
 }
