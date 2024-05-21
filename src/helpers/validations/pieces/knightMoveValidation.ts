a18c3807af84e4492cb28904ebc712956ea071c6
Kyle Close <k.james.close@gmail.com>
2024-05-21 01:00:11 -0700

    knight validations done

--- src/helpers/validations/pieces/knightMoveValidation.ts
+++ src/helpers/validations/pieces/knightMoveValidation.ts
@@ -1,28 +1,67 @@
 import { BoardState, Piece } from '../../../context/board/InitialState';
+import { PieceFile, getPieceFile } from '../../generic/pieceLocation';
 
 export function knightMoveValidation(
   board: BoardState,
   piece: Piece,
   currentIndex: number
 ) {
-  let allSquares = getAllKnightMoves(currentIndex);
-  const filteredOutOfBounds = filterOutOfBounds(allSquares);
-  console.log(filteredOutOfBounds);
+  const pieceFile = getPieceFile(currentIndex);
+
+  let validSquares = getAllKnightMoves(currentIndex, pieceFile);
+  validSquares = filterOutOfBounds(validSquares);
+  return filterOccupiedSelf(board, piece, validSquares);
 }
 
-function getAllKnightMoves(currentIndex: number) {
-  return [
-    currentIndex - 17,
-    currentIndex - 15,
-    currentIndex - 6,
-    currentIndex + 10,
-    currentIndex + 17,
-    currentIndex + 15,
-    currentIndex + 6,
-    currentIndex - 10,
-  ];
+function getAllKnightMoves(currentIndex: number, startFile: PieceFile) {
+  let leftSideTop = null;
+  let leftTop = null;
+  let leftBottom = null;
+  let leftSideBottom = null;
+  let rightSideTop = null;
+  let rightTop = null;
+  let rightSideBottom = null;
+  let rightBottom = null;
+
+  if (startFile !== 'a' && startFile !== 'b') {
+    leftSideTop = currentIndex - 10;
+    leftTop = currentIndex - 17;
+    leftBottom = currentIndex + 15;
+    leftSideBottom = currentIndex + 6;
+  } else if (startFile === 'b') {
+    leftTop = currentIndex - 17;
+    leftBottom = currentIndex + 15;
+  }
+  if (startFile !== 'g' && startFile !== 'h') {
+    rightTop = currentIndex - 15;
+    rightSideTop = currentIndex - 6;
+    rightSideBottom = currentIndex + 10;
+    rightBottom = currentIndex + 17;
+  } else if (startFile === 'g') {
+    rightTop = currentIndex - 15;
+    rightBottom = currentIndex + 17;
+  }
+
+  const result: number[] = [];
+
+  if (leftSideTop) result.push(leftSideTop);
+  if (leftTop) result.push(leftTop);
+  if (leftBottom) result.push(leftBottom);
+  if (leftSideBottom) result.push(leftSideBottom);
+  if (rightTop) result.push(rightTop);
+  if (rightSideTop) result.push(rightSideTop);
+  if (rightSideBottom) result.push(rightSideBottom);
+  if (rightBottom) result.push(rightBottom);
+
+  return result;
 }
 
 function filterOutOfBounds(allSquares: number[]) {
   return allSquares.filter((index) => index >= 0 && index <= 63);
+}
+
+function filterOccupiedSelf(board: BoardState, piece: Piece, validSquares: number[]) {
+  return validSquares.filter(
+    (index) => board[index].piece === null || board[index].piece?.color !== piece.color
+  );
 }
