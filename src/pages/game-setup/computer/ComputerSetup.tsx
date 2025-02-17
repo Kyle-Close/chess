6d2dea908a95b1f300daad72d53eb27061e72a91
Kyle-Close <k.james.close@gmail.com>
2025-02-16 21:58:33 -0800

    yup

--- src/pages/game-setup/computer/ComputerSetup.tsx
+++ src/pages/game-setup/computer/ComputerSetup.tsx
@@ -1,9 +1,8 @@
 import { useGameSettings } from "base/features/game-settings/hooks/useGameSettings";
 import { Box, Button } from "@chakra-ui/react";
-import { AdditionalOptions } from "./AdditionalOptions";
-import { FenInput } from "./FenInput";
-import { PlayerNamesInputs } from "./local/PlayerNamesInputs";
 import { GameType } from "base/redux/slices/gameSettings";
+import { AdditionalOptions } from "../AdditionalOptions";
+import { FenInput } from "../FenInput";
 
 export function ComputerSetup() {
   const gameSettings = useGameSettings(GameType.COMPUTER);
@@ -14,7 +13,6 @@ export function ComputerSetup() {
       className='flex flex-col my-2 gap-4 flex-grow'
       onSubmit={gameSettings.handleSubmit(gameSettings.onFormSubmit)}
     >
-      <PlayerNamesInputs register={gameSettings.register} />
       <AdditionalOptions register={gameSettings.register} />
       <FenInput register={gameSettings.register} />
       <Button mt='auto' type='submit' colorScheme='green'>
